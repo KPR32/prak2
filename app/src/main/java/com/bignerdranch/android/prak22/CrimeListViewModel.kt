b67de5e3865e47aa6e190fd3ab514cc1c0845631
kolyasnikovpr22 <deidara275071>
2022-12-05 13:02:29 +0500

    prak2

---
+++ app/src/main/java/com/bignerdranch/android/prak22/CrimeListViewModel.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.prak22
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/prak22/database/CrimeDao.kt
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.prak22.database
+
+import androidx.lifecycle.LiveData
+import java.util.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.bignerdranch.android.prak22.Crime
+
+@Dao
+interface CrimeDao {
+    @Query("SELECT * FROM crime")
+    fun getCrimes(): LiveData<List<Crime>>
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): LiveData<Crime?>
+    @Update
+    fun updateCrime(crime: Crime)
+    @Insert
+    fun addCrime(crime: Crime)
+}
